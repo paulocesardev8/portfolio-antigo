2a822d0949d9a50f11e5471eb44716d69bb61eb4
Paulo Cesar <paulo.pereira.jr.s@gmail.com>
2026-01-22 14:12:56 -0300

    Atualização landing page vendas

--- components/VME.tsx
+++ components/VME.tsx
@@ -2,7 +2,7 @@ export default function VME() {
   return (
     <section className="bg-gray-800 text-white px-6 py-12 mt-24 rounded-xl shadow-xl w-full text-center max-w-4xl mx-auto">
       <p className="text-2xl sm:text-3xl font-bold leading-tight mb-4">
-        🚀 Mais de <span className="text-blue-400">300 alunos</span> já aplicaram e viram resultados reais.
+        🚀 Mais de <span className="text-blue-400">500 alunos</span> já aplicaram e viram resultados reais.
       </p>
       <p className="text-lg text-gray-300 max-w-2xl mx-auto">
         Resultados conquistados com estratégias atualizadas, suporte próximo e uma didática que simplifica até o que parece complexo.
